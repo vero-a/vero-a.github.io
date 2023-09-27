2699c792dcf0dc41d8081b7cb1299af4fb86fea2
Veroneca Abou faissal <veroneca@Veronecas-MacBook-Pro.local>
2023-09-27 13:13:43 -0400

    edited contact title

--- pages/components/Contact.jsx
+++ pages/components/Contact.jsx
@@ -5,7 +5,7 @@ import { AiOutlineMail } from 'react-icons/ai';
 function Contact() {
   return (
     <div id='contact' className='px-10 pb-10'>
-        <h2 className='text-2xl tracking-widest uppercase text-center text-emerald-500 py-10 mx-4 md:text-3xl dark:text-emerald-500'>Let&apos;s get in touch</h2>
+        <h2 className='text-2xl tracking-widest uppercase text-center text-emerald-500 py-10 mx-4 md:text-3xl dark:text-emerald-500'>Let&apos;s get in touch!</h2>
             <div>
                 <ul className='flex text-3xl mx-4 gap-5 justify-center'>
                     <li className='rounded-md shadow-md shadow-gray-400 p-2 hover:scale-105 dark:text-slate-200 dark:shadow-gray-700'>
